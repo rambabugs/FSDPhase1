f9e4283baeb13902e85f46c58a2eebe3fad8e4d6
gsram <sidarthram888@gmail.com>
2022-06-17 20:49:44 +0530

    initial commit

---
+++ src/com/gsrambabu/Main.java
@@ -0,0 +1,17 @@
+package com.gsrambabu;
+
+import pkg2.employee;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.println("Hello world");
+
+        employee emp = new employee();
+
+
+
+    }
+}
---
+++ src/pkg2/employee.java
@@ -0,0 +1,13 @@
+package pkg2;
+
+public class employee {
+
+    String carName;
+    String Colour;
+
+    void caremp(){
+
+    }
+
+
+}
